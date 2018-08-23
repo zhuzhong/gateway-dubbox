3685b28a0da0d524db353a758afaff0f0e5b57c3
zhuzhong <zhuzhong@AladingBank.com>
2018-08-23 16:00:28 +0800

    增加string编码说明

--- gateway-core/src/main/java/com/z/gateway/core/OpenApiHttpClientService.java
+++ gateway-core/src/main/java/com/z/gateway/core/OpenApiHttpClientService.java
@@ -12,8 +12,14 @@ import java.util.Map;
  */
 public interface OpenApiHttpClientService {
 
-    // get请求
-    public String/*string 使用base64编码，使用时需要base64解码*/ doGet(String webUrl,Map<String,String> requestHeader);
+    
+    /*
+         返回值string 使用base64编码，使用时需要base64解码
+            之前使用 String 的utf-8 编解码，对于文本类型是没有问题;但是对于image 类型的支持就会有问题，原因是它是错误的编码，后面再解码就不正确了，
+            后来该这些接口改为使用字码码byte[]的形式作为返回值，但是总觉得不太好，所以最终使用base64进行字符串的编解码.对于字节码的编码的细节看来还有很多需要学习
+     
+     */
+    public String doGet(String webUrl,Map<String,String> requestHeader);
     public String doGet(String webUrl, Map<String,String> paramMap,Map<String,String> requestHeader);
     public String doHttpsGet(String webUrl,Map<String,String> requestHeader);
     
