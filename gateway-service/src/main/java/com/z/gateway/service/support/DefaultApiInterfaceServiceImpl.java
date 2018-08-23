e208d105e65d574eb7c5a688b726e2430fda0b46
zhuzhong <zhuzhong@AladingBank.com>
2018-08-23 15:11:52 +0800

    测试api

--- gateway-service/src/main/java/com/z/gateway/service/support/DefaultApiInterfaceServiceImpl.java
+++ gateway-service/src/main/java/com/z/gateway/service/support/DefaultApiInterfaceServiceImpl.java
@@ -15,7 +15,7 @@ public class DefaultApiInterfaceServiceImpl implements ApiInterfaceService {
 
     @Override
     public ApiInterface queryApiInterfaceByApiId(String apiId, String version) {
-        
+   
         return null;
     }
 
